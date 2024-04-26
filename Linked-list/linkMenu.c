b624b984bbfcbe8aa0993a6ee7b891b41a6b7285
Abhishek Gupta <abhishekgupta0301@gmail.com>
2024-04-26 22:47:22 +0530

    insert from begining and last

--- Linked-list/linkMenu.c
+++ Linked-list/linkMenu.c
@@ -7,18 +7,85 @@
  struct node *head;
  void beginsert ();   
 void lastinsert ();  
-void randominsert();  
-void begin_delete();  
-void last_delete();  
-void random_delete();  
-void display();  
-void search();
-void main()
+// void randominsert();  
+// void begin_delete();  
+// void last_delete();  
+// void random_delete();  
+// void display();  
+// void search();
+int main()
 {
   int choice=0;
   while(choice !=9)
   {
     printf("\n\n***********Main Menu*************\n");
-    printf("choose one option from the following lis ...\n");
+    printf("choose one option from the following list ...\n");
+    printf("\n===============================================\n");
+    printf("\n1.insert in begining\n2.insert at the last");
+    printf("\nenter your choice?\n");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+      case 1:
+      beginsert();
+      break;
+      case 2:
+      lastinsert();
+      break;
+      default:
+      printf("please enter valid choice");
+    }
+  }
+   return 0;
+}
+
+
+void beginsert(){
+  struct node *new1;
+  int item;
+  new1=(struct node*)malloc(sizeof(struct node*));
+  if(new1==NULL)// suppose memory available nahi hui 
+  {
+    printf("\n overflow ");
+  }
+  else{
+    printf("\n Emter value\n");
+    scanf("%d",&item);
+    new1->data=item;
+    new1->next=head;
+    head=new1;
+    printf("\n Node inserted ");
   }
 }
+ void lastinsert (){
+  struct node *new1,*temp;
+  int item;
+  new1=(struct node*)malloc(sizeof(struct node*));
+  if(new1==NULL) // memory nahi allocate hui
+  {
+    printf("\nOVERFLOW");
+  }
+  else{
+    printf("\n Enter the value");
+    scanf("%d",&item);
+    new1->data=item;
+    if(head==NULL)// koi node ho hi na to
+    {
+      new1->next=NULL;
+    head=new1;
+    printf("\nNode inserted" );
+    }
+    else{
+      temp=head;
+      while(temp->next !=NULL)
+      {
+        temp=temp->next;
+      }
+      temp->next=new1;
+      new1->next=NULL;
+      printf("\nNode inserted");
+    }
+  }
+  }
+
+ 
