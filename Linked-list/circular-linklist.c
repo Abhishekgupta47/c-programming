15545357e89c810bf41e3fce9fcd26bb43d483ca
Abhishek Gupta <abhishekgupta0301@gmail.com>
2024-05-26 00:48:08 +0530

    circular linked list

--- Linked-list/circular-linklist.c
+++ Linked-list/circular-linklist.c
@@ -6,7 +6,8 @@ struct node{
 };
 struct node *head;
 void beginsert();
-void lastinser();
+void lastinsert();
+void begindelete();
 void main()
 {
   int choice;
@@ -14,7 +15,7 @@ void main()
   {
   printf("\n*********MENU**********\n");
   printf("\n choose one option from the following list...\n");
-  printf("\n1.insert at begining\n2.insert at last");
+  printf("\n1.insert at begining\n2.insert at last\n3.delete at begining");
   printf("\nenter choice\n");
   scanf("\n%d",&choice);
   switch(choice)
@@ -23,7 +24,11 @@ void main()
     beginsert();
     break;
     case 2:
-    lastinser();
+    lastinsert();
+    break;
+    case 3:
+    begindelete();
+    break;
     default:
     printf("\nenter valid choice\n");
   }
@@ -41,7 +46,7 @@ void beginsert()
     printf("\nOVERFLOW\n");
   }
   else{
-    printf("\n enter the node data ?");
+    printf("\n enter the node data ?\n");
     scanf("%d",&item);
     new1->data=item;
     if(head==NULL)
@@ -92,3 +97,31 @@ void lastinsert()
   }
   
 }
+ void begindelete()
+ {
+  struct node *temp;
+  if(head==NULL)
+  {
+    printf("\nUNDERFLOW");
+  }
+  else if(head->next==head)
+  {
+    head==NULL;
+    free(head);
+    printf("\n node deleted\n");
+
+  }
+  else
+  {
+    temp=head;
+    while(temp->next!=NULL)
+    
+       temp=temp->next;
+       temp->next=head->next;
+       free(head);
+       head=temp->next;
+       printf("\nnode deleted\n");
+    
+  }
+ }
+ 
